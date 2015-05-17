50aa10c50b9f2f9bd962b927dfe643b59a3a6eb2
ngentile <ngentile@arbor.net>
2015-05-17 17:37:22 -0400

    implement issues resource

---
+++ api/issues.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from ..http import (
+    GET,
+    PUT
+)
+from .projects import PATH as PROJECTPATH
+from .base import PATH as BASEPATH
+
+PATH = BASEPATH + "/issues"
+PROJECT_ISSUES_PATH = PROJECTPATH + "/issues"
+PROJECT_ISSUE_PATH = PROJECT_ISSUES_PATH + "/{issue_id}"
+
+
+@GET
+def list(**kwargs):
+    """List issues"""
+    return PATH, kwargs
+
+
+@GET
+def project(**kwargs):
+    """List project issues"""
+    return PROJECT_ISSUES_PATH, kwargs
+
+
+@GET
+def single(**kwargs):
+    """Single issue"""
+    return PROJECT_ISSUE_PATH, kwargs
+
+
+@GET
+def create(**kwargs):
+    """New issue"""
+    return PROJECT_ISSUES_PATH, kwargs
+
+
+@PUT
+def edit(**kwargs):
+    """Edit issue"""
+    return PROJECT_ISSUE_PATH, kwargs
