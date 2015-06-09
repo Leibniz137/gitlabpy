2e408ae32832ff006d145cba44f3404daac369dc
ngentile <ngentile@arbor.net>
2015-06-09 11:50:24 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='gitlabpy',
+    version='0.1.1',
+    description='Gitlab for humans',
+    author='Nathaniel Gentile',
+    author_email='ncg09@hampshire.edu',
+    package_dir={'gitlabpy': ''},
+    packages=['gitlabpy'])
