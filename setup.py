fc97b76e618e8810f233bc1e9eabd5cff69093e3
ngentile <ngentile@arbor.net>
2015-07-02 16:56:57 -0400

    specify requests package as a dependency

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from distutils.core import setup
 
 setup(
     name='gitlabpy',
-    version='0.1.1',
+    version='0.1.2',
     description='Gitlab for humans',
     author='Nathaniel Gentile',
     author_email='ncg09@hampshire.edu',
     package_dir={'gitlabpy': ''},
-    packages=['gitlabpy', 'gitlabpy.api'])
+    packages=['gitlabpy', 'gitlabpy.api'],
+    install_requires=['requests'])
