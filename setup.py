42a84eb98340a19ff3ed6dec345492fa34f81865
ngentile <ngentile@arbor.net>
2015-07-07 18:02:41 -0400

    increment version number to 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='gitlabpy',
-    version='0.1.2',
+    version='0.2.0',
     description='Gitlab for humans',
     author='Nathaniel Gentile',
     author_email='ncg09@hampshire.edu',
